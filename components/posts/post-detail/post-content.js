d25174bb0427c82bde708dca194e8053f1d5e203
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-05-04 22:08:14 -0400

    Feat. Rendering code snippets from MD files

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,8 @@
 import React from "react";
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
@@ -8,10 +11,53 @@ function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customComponents = {
+    // image(image) {
+    //   return <Image
+    //     src={`images/posts/${post.slug}/${image.src}`}
+    //     alt={image.alt}
+    //     width={600}
+    //     height={300} />
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0]; // this version of syntax highlighter yields the language as 'language-js', therefore we only need the 'js' part.
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+
+      return (
+        <SyntaxHighlighter
+          language={language}
+          style={nightOwl}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 }
