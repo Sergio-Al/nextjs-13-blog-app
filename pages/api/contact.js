7240661c6fdc02c1823d96352a3ef87a66781e94
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-05-05 11:29:38 -0400

    Feat. Adding a MongoDB for contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -14,14 +16,34 @@ function handler(req, res) {
       return;
     }
 
-    // Store in a DB
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        `${process.env.NEXT_PUBLIC_MONGODB_URI}`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect the database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    } finally {
+      client.close();
+    }
 
     res
       .status(201)
